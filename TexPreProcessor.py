77745488f9051842674c7e64afb5623c9cd3b601
Johannes Bauer <JohannesBauer@gmx.de>
2016-07-25 20:28:27 +0200

    Moving from dict to sorted list for O(n) performance

--- TexPreProcessor.py
+++ TexPreProcessor.py
@@ -21,17 +21,60 @@
 #	Johannes Bauer <joe@johannes-bauer.com>
 #
 
+import re
+import bisect
+
 class _TextFragment(object):
-	def __init__(self, text, startoffset = offset):
+	def __init__(self, text):
 		self._text = text
-		self._offset = offset
+		self._offsetmap = [ [ 0, 0 ] ]
 
 	@property
 	def text(self):
 		return self._text
 
+	def delete_span(self, span):
+		(span_from, span_to) = span
+		self._text = self._text[ : span_from] + self._text[span_to : ]
+
+		translated_from = self.translate_offset(span_from)
+		increase = span_to - span_from
+		if translated_from not in self._offsetmap:
+			self._offsetmap[translated_from] = translated_from + increase
+#		for (src_offset, dst_offset) in self._offsetmap.items():
+#			if src_offset > span_from:
+#				self._offsetmap[src_offset] += increase
+
+	def delete_regex(self, regex):
+		regex = re.compile(regex)
+		deletions = [ ]
+		for deletion in regex.finditer(self.text):
+			deletions.append(deletion.span())
+		for span in reversed(deletions):
+			self.delete_span(span)
+			break
+
+	def translate_offset(self, qry_offset):
+		index = bisect.bisect_left(self._offsetmap, [ qry_offset, 0 ])
+		span = self._offsetmap[index]
+		distance = span[1] - span[0]
+		return qry_offset + distance
+		print(index)
+
+		mapped_offset = 0
+		for (src_offset, dst_offset) in sorted(self._offsetmap.items()):
+			if qry_offset >= src_offset:
+				mapped_offset = dst_offset - src_offset
+			else:
+				break
+		return qry_offset + mapped_offset
+
+	def dump(self):
+		for (offset, char) in enumerate(self.text):
+			print("%2d %s" % (self.translate_offset(offset), char))
+
 	def __str__(self):
-		return "\"%s\"" % (self._text)
+		return "\"%s\"" % (self.text)
 
 class TexPreprocessor(object):
 	def __init__(self, texfilename):
@@ -45,5 +88,16 @@ class TexPreprocessor(object):
 
 
 if __name__ == "__main__":
-	text = _TextFragment("foobar")
-	print(text)
+#	with open("test.tex") as f:
+#		text = f.read()
+
+	text = _TextFragment("Numbers at eleven and another at 33 at at 42.")
+	text.dump()
+	print("-" * 120)
+	for i in range(4):
+		text.delete_regex("at ")
+		text.dump()
+		print(text._offsetmap)
+		print()
+
+
