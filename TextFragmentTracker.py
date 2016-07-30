2afee72dee9841618ebc05460dab2407ed502eaf
Johannes Bauer <JohannesBauer@gmx.de>
2016-07-30 14:26:28 +0200

    One testcase fixed

---
+++ TextFragmentTracker.py
@@ -0,0 +1,193 @@
+#
+#	biblint - Static checking of BibTeX files to find errors or inconsistencies.
+#	Copyright (C) 2016-2016 Johannes Bauer
+#
+#	This file is part of biblint.
+#
+#	biblint is free software; you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation; this program is ONLY licensed under
+#	version 3 of the License, later versions are explicitly excluded.
+#
+#	biblint is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with biblint; if not, write to the Free Software
+#	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#	Johannes Bauer <joe@johannes-bauer.com>
+#
+
+import re
+import bisect
+
+class TextFragmentTracker(object):
+	"""Memorizes original offsets in modified pieces of text. This is useful
+	because it allows us to pre-process the given TeX files (e.g., remove
+	\emph{foo} fragments and replace them just by "foo" in order to make
+	grammatical analysis easier/possible. However, we want to track the
+	original positions in the file so we can later on -- once we find
+	lint-worthy offenses, pinpoint their line/column number."""
+
+	_debug = False
+
+	def __init__(self, text):
+		self._text = text
+		self._offsetmap = [ [ 0, 0 ] ]
+
+	def _assert_offsetmap_integrity(self):
+		last_src = self._offsetmap[0][0]
+		if not last_src == 0:
+			raise AssertionError("offsetmap[0][0] is not starting with zero index.")
+		for (index, (src, dst)) in enumerate(self._offsetmap[1:]):
+			if src <= last_src:
+				raise AssertionError("offsetmap is not strictly monotonically increasing, error at index %d: %s" % (index + 1, str(self._offsetmap)))
+			last_src = src
+
+	@property
+	def text(self):
+		return self._text
+
+	def delete_span(self, span):
+		"""Deletes a span from the text."""
+
+		# Assign some helper variables first
+		(span_from, span_to) = span
+		length = span_to - span_from
+		if __debug__ and self._debug:
+			print("Removing span [ %d; %d ] length %d" % (span_from, span_to, length))
+
+		# Remove the span in the actual text
+		removed_text = self._text[span_from : span_to]
+		if __debug__ and self._debug:
+			print("Removed: '%s'" % (removed_text))
+		self._text = self._text[ : span_from] + self._text[span_to : ]
+		if __debug__ and self._debug:
+			print("Remaining: '%s'" % (self._text))
+
+		# Then find out the current translated offset
+		(index_from, translated_from) = self.translate_full_offset(span_from)
+		(index_to, translated_to) = self.translate_full_offset(span_to)
+		if __debug__ and self._debug:
+			print("Previously: offset %d is idx %d-%d (%s - %s), translated_offset end = %d" % (span_to, index_from, index_to, self._offsetmap[index_from], self._offsetmap[index_to], translated_to))
+
+		# Remove old spans first
+		if __debug__ and self._debug:
+			print("Remove span PRE ", self._offsetmap)
+		self._offsetmap = self._offsetmap[ : index_from + 1 ] + self._offsetmap[index_to + 1 : ]
+		if __debug__ and self._debug:
+			print("Remove span POST", self._offsetmap)
+
+		# Then insert new one
+		insert_index = index_from + 1
+		new_element = [ span_from, translated_to ]
+		if __debug__ and self._debug:
+			print("New element: %s" % (new_element))
+
+		if self._offsetmap[index_from][0] == new_element[0]:
+			if __debug__ and self._debug:
+				old_element = self._offsetmap[index_from]
+				print("Updating index of element at %d: %s -> %s" % (insert_index, old_element, new_element))
+			self._offsetmap[index_from] = new_element
+			start_traversal_index = insert_index
+		else:
+			if __debug__ and self._debug:
+				print("Inserting new element at %d: %s" % (insert_index, new_element))
+			self._offsetmap.insert(insert_index, new_element)
+			start_traversal_index = insert_index + 1
+
+		for i in range(start_traversal_index, len(self._offsetmap)):
+			self._offsetmap[i][0] -= length
+
+		if __debug__ and self._debug:
+			print("After linear traversal starting from %d: %s" % (start_traversal_index, self._offsetmap))
+
+		if __debug__ and self._debug:
+			# Only during debugging, check list after every modification
+			self._assert_offsetmap_integrity()
+			print()
+
+	def replace_span(self, span, replacement):
+		"""Deletes a span from the text and replaces it by the given
+		replacement string."""
+		self.delete_span(span)
+		(span_from, span_to) = span
+		self._text = self._text[ : span_from] + replacement + self._text[span_from : ]
+
+	@staticmethod
+	def _mkregex(regex):
+		if isinstance(regex, str):
+			return re.compile(regex)
+		else:
+			return regex
+
+	@staticmethod
+	def _evalpattern(occurence, replacement):
+		if isinstance(replacement, str):
+			return replacement
+		else:
+			return replacement(occurence)
+
+	def	replace_regex(self, regex, replacement):
+		"""Searches the text for a given regex (can also be a string, will then
+		be compiled on-the-fly) and replaces every occurence by the given
+		replacement (can be a lambda which is passed the regex match or a
+		static string as well.)"""
+		regex = self._mkregex(regex)
+		replacements = [ ]
+		for occurence in regex.finditer(self.text):
+			replpattern = self._evalpattern(occurence, replacement)
+			replacements.append((occurence.span(), replpattern))
+		for (span, replpattern) in reversed(replacements):
+			self.replace_span(span, replpattern)
+
+	def delete_regex(self, regex, delete_spans = None):
+		"""Deletes the given regex from the text. The optional delete_spans
+		argument can be specified and needs to contain a number of sub-span
+		indicies which are to be deleted. For example, if you delete
+		(.*)foobar(A+) and pass delete_spans = [ 2 ], only the (A+) will be
+		deleted from text."""
+		if delete_spans is None:
+			delete_spans = [ 0 ]
+		regex = self._mkregex(regex)
+		deletions = [ ]
+		for occurence in regex.finditer(self.text):
+			for delspan in delete_spans:
+				deletions.append(occurence.span(delspan))
+		for span in reversed(deletions):
+			self.delete_span(span)
+
+	def translate_offset_index(self, qry_offset):
+		index = bisect.bisect_left(self._offsetmap, [ qry_offset + 1, 0 ]) - 1
+		if index == -1:
+			index = 0
+		return index
+
+	def translate_full_offset(self, qry_offset):
+		index = self.translate_offset_index(qry_offset)
+		span = self._offsetmap[index]
+		distance = span[1] - span[0]
+		return (index, qry_offset + distance)
+
+	def translate_offset(self, qry_offset):
+		"""Translates an offset within the text into its original location offset."""
+		return self.translate_full_offset(qry_offset)[1]
+
+	def dump(self):
+		for (offset, char) in enumerate(self.text):
+			(index, xoffset) = self.translate_full_offset(offset)
+			print("%2d %2d %3d %s" % (offset, index, xoffset, char))
+
+	def __len__(self):
+		return len(self.text)
+
+	def __iter__(self):
+		for (offset, pos) in enumerate(self.text):
+			yield (self.translate_offset(offset), pos)
+
+	def __str__(self):
+		return "\"%s\"" % (self.text)
+
---
+++ TextFragmentTrackerTests.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python3
+#
+#	biblint - Static checking of BibTeX files to find errors or inconsistencies.
+#	Copyright (C) 2016-2016 Johannes Bauer
+#
+#	This file is part of biblint.
+#
+#	biblint is free software; you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation; this program is ONLY licensed under
+#	version 3 of the License, later versions are explicitly excluded.
+#
+#	biblint is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with biblint; if not, write to the Free Software
+#	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#	Johannes Bauer <joe@johannes-bauer.com>
+#
+
+import unittest
+
+from TextFragmentTracker import TextFragmentTracker
+
+class TextFragmentTrackerTests(unittest.TestCase):
+	def test_simple(self):
+		# Simple test case
+		text = TextFragmentTracker("AaBbCcDdEe")
+		text._offsetmap = [ [ 0, 0 ], [ 2, 10 ], [ 4, 20 ], [ 6, 30 ], [ 8, 40 ] ]
+		self.assertEqual(list(text), [(0, 'A'), (1, 'a'), (10, 'B'), (11, 'b'), (20, 'C'), (21, 'c'), (30, 'D'), (31, 'd'), (40, 'E'), (41, 'e')])
+
+	def test_removal_within_spans(self):
+		# Regular test case with removals within spans
+		text = TextFragmentTracker("Foo^^bar Moo^^koo Blah^^Blubb")
+		text._offsetmap = [ [ 0, 0 ], [ 9, 100 ], [ 18, 200 ] ]
+		self.assertEqual(list(text), [(0, 'F'), (1, 'o'), (2, 'o'), (3, '^'), (4, '^'), (5, 'b'), (6, 'a'), (7, 'r'), (8, ' '), (100, 'M'), (101, 'o'), (102, 'o'), (103, '^'), (104, '^'), (105, 'k'), (106, 'o'), (107, 'o'), (108, ' '), (200, 'B'), (201, 'l'), (202, 'a'), (203, 'h'), (204, '^'), (205, '^'), (206, 'B'), (207, 'l'), (208, 'u'), (209, 'b'), (210, 'b')])
+		text.delete_span((3, 5))
+		self.assertEqual(list(text), [(0, 'F'), (1, 'o'), (2, 'o'), (5, 'b'), (6, 'a'), (7, 'r'), (8, ' '), (100, 'M'), (101, 'o'), (102, 'o'), (103, '^'), (104, '^'), (105, 'k'), (106, 'o'), (107, 'o'), (108, ' '), (200, 'B'), (201, 'l'), (202, 'a'), (203, 'h'), (204, '^'), (205, '^'), (206, 'B'), (207, 'l'), (208, 'u'), (209, 'b'), (210, 'b')])
+		text.delete_span((10, 12))
+		self.assertEqual(list(text), [(0, 'F'), (1, 'o'), (2, 'o'), (5, 'b'), (6, 'a'), (7, 'r'), (8, ' '), (100, 'M'), (101, 'o'), (102, 'o'), (105, 'k'), (106, 'o'), (107, 'o'), (108, ' '), (200, 'B'), (201, 'l'), (202, 'a'), (203, 'h'), (204, '^'), (205, '^'), (206, 'B'), (207, 'l'), (208, 'u'), (209, 'b'), (210, 'b')])
+		text.delete_span((18, 20))
+		self.assertEqual(list(text), [(0, 'F'), (1, 'o'), (2, 'o'), (5, 'b'), (6, 'a'), (7, 'r'), (8, ' '), (100, 'M'), (101, 'o'), (102, 'o'), (105, 'k'), (106, 'o'), (107, 'o'), (108, ' '), (200, 'B'), (201, 'l'), (202, 'a'), (203, 'h'), (206, 'B'), (207, 'l'), (208, 'u'), (209, 'b'), (210, 'b')])
+
+	def test_removal_beginning(self):
+		# Test case with removal at the beginning
+		text = TextFragmentTracker("Foobar")
+		self.assertEqual(list(text), [(0, 'F'), (1, 'o'), (2, 'o'), (3, 'b'), (4, 'a'), (5, 'r')] )
+		text.delete_span((0, 3))
+		self.assertEqual(list(text), [(3, 'b'), (4, 'a'), (5, 'r')] )
+
+	def test_removal_multiple_spans_1(self):
+		# Test case with removal over multiple spans
+		text = TextFragmentTracker("FooXYZBar")
+		text._offsetmap = [ [0, 0], [ 3, 100 ],[ 4, 200 ],[ 5, 300 ], [6, 1000] ]
+		self.assertEqual(list(text), [(0, 'F'), (1, 'o'), (2, 'o'), (100, 'X'), (200, 'Y'), (300, 'Z'), (1000, 'B'), (1001, 'a'), (1002, 'r')])
+		text.delete_span((3, 6))
+		self.assertEqual(list(text), [(0, 'F'), (1, 'o'), (2, 'o'), (1000, 'B'), (1001, 'a'), (1002, 'r')])
+
+	def test_removal_multiple_spans_2(self):
+		# Test case with removal over multiple spans not starting in one
+		text = TextFragmentTracker("FooBarKoo")
+		text._offsetmap = [ [ 0, 0 ], [3, 100], [4, 200], [5, 300], [6, 1000] ]
+		self.assertEqual(list(text), [(0, 'F'), (1, 'o'), (2, 'o'), (100, 'B'), (200, 'a'), (300, 'r'), (1000, 'K'), (1001, 'o'), (1002, 'o')])
+		text.delete_span((1, 7))
+		self.assertEqual(text._offsetmap, [ [ 0, 0 ], [ 1, 1001 ] ])
+		self.assertEqual(list(text), [(0, 'F'), (1001, 'o'), (1002, 'o')])
+
+	def test_numbers(self):
+		# Number test
+		text = TextFragmentTracker("Numbers at eleven and another at 33 at at 42.")
+		self.assertEqual(text.translate_offset(text.text.index("33")), 33)
+		self.assertEqual(text.translate_offset(text.text.index("42")), 42)
+		text.delete_regex("at ")
+		self.assertEqual(text.translate_offset(text.text.index("33")), 33)
+		self.assertEqual(text.translate_offset(text.text.index("42")), 42)
+
+	def _assert_integrity(self, text):
+		assert(all(pos % 10 == int(char) for (pos, char) in text))
+
+	def test_long_text(self):
+		# Remove long text
+		text = TextFragmentTracker("0123456789" * 3)
+		text.delete_regex("012345")
+		assert_integrity(text)
+
+	def test_remove_head(self):
+		# Head remove test
+		TextFragmentTracker._debug = True
+		text = TextFragmentTracker("0123456789" * 3)
+
+		text.delete_regex("012345678")
+		self._assert_integrity(text)
+
+	def test_remove_multiple(self):
+		# Multiple remove test
+		text = TextFragmentTracker("0123456789" * 10)
+		text.delete_regex("7")
+		assert_integrity(text)
+		text.delete_regex("2.4")
+		assert_integrity(text)
+		text.delete_regex("0")
+		assert_integrity(text)
+	#	text.delete_regex("915")
+	#	assert_integrity(text)
+	#	text.delete_regex("15686")
+	#	assert_integrity(text)
+
+if __name__ == "__main__":
+	unittest.main()
+
+
+
